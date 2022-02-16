db8c22b3f034c8a84fa876579cbee899ec7b0497
williamb2147 <99830383+williamb2147@users.noreply.github.com>
2022-02-16 14:17:13 -0500

    Add files via upload
    P1HW2-Pizza Order

---
+++ P1HW2_WilliamsBert.py
@@ -0,0 +1,18 @@
+#P1HW2 Pizza order
+#2/16/2022
+#CTI-110 P1HW2-Pizza Order
+#Williams, Bert
+#
+print('Please enter number of student.')
+guest_num=int(input())
+slice_num=int(input())
+needed_num=slice_num*3
+pizza_num=needed_num/6
+print('------Pizza Order------')
+print('Number of Students:',guest_num)
+print('Pizza Slices Needed:',needed_num)
+print('Pizzas Needed:',pizza_num)
+print('-----------------------')
+
+
+
